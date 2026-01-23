a6d354bb8d4b0e683ac5b9fdb6f564570cdfffc3
aserlite <arthurcuvillon691@gmail.com>
2026-01-23 19:11:38 +0100

    jsuis con

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/aserlite/pastoplesfilms',
+  base: 'https://aserlite.github.io/pastoplesfilms/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
